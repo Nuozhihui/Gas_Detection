11c7ecd4ee0f336f821187915204aac32314d441
Nuozhihui <1285574579@qq.com>
2022-04-11 22:02:06 +0800

    V0.1
    1.可以实现 步进 前进后退
    2.湿度 气体报警
    3.LCD显示
    4.继电器控制

---
+++ HA/ADC/Gas.h
@@ -0,0 +1,12 @@
+
+#ifndef __GAS_H__
+#define __GAS_H__
+
+
+#include<reg51.h>
+
+
+uint ReadAD(uchar cmd);
+
+
+#endif
---
+++ HA/STEP/STEP.h
@@ -0,0 +1,12 @@
+
+#ifndef __GAS_H__
+#define __GAS_H__
+
+
+#include<reg51.h>
+
+void Go_Step(void);
+void Break_Step(void);
+
+
+#endif
